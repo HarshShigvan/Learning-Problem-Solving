1f0621676773de901e926381ce8146561e727aa7
Harsh Shigvan <harsh.shigvan.cse@gmail.com>
2026-04-23 22:50:15 +0530

    DSA foundation one_day

---
+++ 2.Programming + DSA Basic/one_day.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+public class one_day {
+  public static void main(String[] args) {
+// problem 1
+    
+    // Scanner sc = new Scanner(System.in);
+
+
+    // System.out.println("Enter a number 1 : ");
+    // int num1 = sc.nextInt();
+
+    // System.out.println("Enter a number 2 : ");
+    // int num2 = sc.nextInt();
+
+    // System.out.println(num1);
+    // System.out.println(num2);
+// // problem 2
+    // Scanner sc = new Scanner(System.in);
+
+    // System.out.println("Enter a first name : ");
+    // String name1 = sc.nextLine();
+
+    // System.out.println("Enter a last name : ");
+    // String name2 = sc.nextLine();
+
+    // System.out.println(name1 + " " + name2);
+
+
+// // problem 3
+// Scanner sc = new Scanner(System.in);
+
+// System.out.println("Enter a number 1 : ");
+// int num1 = sc.nextInt();
+
+// System.out.println("Enter a number 2 : ");
+// int num2 = sc.nextInt();
+
+// System.out.println(num1+num2);
+
+// // Pattern 1
+// for ( int i =1; i< 5; i++) {
+//   for (int j = 1; j<=i; j++) {
+//     System.out.print("*");
+//   }
+//   System.out.println();
+// }
+
+// //Pattern 2
+for (int i = 1; i <= 4; i++) {
+  for (int j = 1; j <= i; j++){
+    System.out.print(j);
+  }
+  System.out.println();
+}
+      }
+}
