80b5a89a26b8e490eee841a98c3e02d03a6dfce6
Harsh Shigvan <harsh.shigvan.cse@gmail.com>
2026-04-27 23:40:46 +0530

    second day file

---
+++ 2.Programming + DSA Basic/two_day.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class two_day {
+  public static void main(String[] args) {
+//     for(int i=1;i<=10;i++){
+//     System.out.println(i);
+// }
+
+// for (int i = 1; i<=20; i++) {
+//   if (i % 2 == 0) {
+//     System.out.println(i);
+//   }
+// }
+
+// Scanner sc = new Scanner(System.in);
+
+// System.out.println("Enter number : ");
+// int num = sc.nextInt();
+
+// for (int i =1; i<=10; i++) {
+//   System.out.println(num + "x" + i + " = " + num*i);
+// }
+
+// Scanner sc = new Scanner(System.in);
+
+// System.out.println("Enter a number : ");
+// int num = sc.nextInt();
+// int harsh = 0;
+// for (int i = 1; i<=num; i++) {
+//   harsh = i+harsh;
+// }
+// System.out.println(harsh);
+  
+for (int i =1; i<=4; i++) {
+  for (int j= 0; j<i; j++) {
+    System.out.print(i);
+  }
+  System.out.println();
+}
+  }
+
+}
